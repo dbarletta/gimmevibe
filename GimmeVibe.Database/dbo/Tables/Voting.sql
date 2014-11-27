2c6231546da7243d1d000fba1530bc945f78ca62
alemarsalinas <alemar.salinas@yahoo.com>
2014-11-27 13:20:01 -0300

    create database project

---
+++ GimmeVibe.Database/dbo/Tables/Voting.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Voting] (
+    [idTransaction] BIGINT         IDENTITY (1, 1) NOT NULL,
+    [IdVoter]       BIGINT         NOT NULL,
+    [Emotion]       NVARCHAR (500) NOT NULL,
+    [Location]      NVARCHAR (500) NOT NULL,
+    [Code]          NVARCHAR (50)  NOT NULL,
+    [VotationTime]  DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Voting] PRIMARY KEY CLUSTERED ([idTransaction] ASC)
+);
+
