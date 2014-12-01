27dd3b5abfa4c3beebe2931b78f2606a4dcc76eb
dbarletta <damian.barletta@gmail.com>
2014-12-01 19:11:21 -0300

    conflict fixed

--- GimmeVibe/scripts/app/controllers/results.js
+++ GimmeVibe/scripts/app/controllers/results.js
@@ -1,5 +1,5 @@
 ﻿angular.module('gvibe.controllers')
 
 .controller('ResultsCtrl', function ($scope, $timeout, $ionicModal, $ionicActionSheet) {
-    
+
 })
---
+++ GimmeVibe/scripts/app/controllers/vote.js
@@ -0,0 +1,11 @@
+﻿angular.module('gvibe.controllers')
+
+.controller('VotesCtrl', function ($scope, $ionicPopup, $state, vote, places) {
+
+    places.getNearbyPlaces();
+
+    $scope.selectEmotion = function (emotion) {
+        vote.emotion = emotion;
+        $state.go('home.voteinfo');
+    }
+})
