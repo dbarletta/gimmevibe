a91d759a1de7bbe13b81f12036f5159f6b5f7a4b
dbarletta <damian.barletta@gmail.com>
2014-12-02 18:59:49 -0300

    delete and ignore /bld

--- GimmeVibe/bld/Ripple/Android/Debug/scripts/app/gvibe.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('gvibe', ['gvibe.config', 'gvibe.services', 'gvibe.controllers', 'gvibe.directives', 'ionic']);
-angular.module('gvibe.config', []);
-angular.module('gvibe.directives', []);
-angular.module('gvibe.controllers', []);
-angular.module('gvibe.services', ['ngResource']);
-
