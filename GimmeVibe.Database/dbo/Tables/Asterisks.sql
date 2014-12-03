0691590503fd403eaa687498ae5783703232bab3
dbarletta <damian.barletta@gmail.com>
2014-12-03 14:04:44 -0300

    database creation and added FKs

--- GimmeVibe.Database/dbo/Tables/Asterisks.sql
+++ GimmeVibe.Database/dbo/Tables/Asterisks.sql
@@ -1,7 +1,10 @@
-﻿CREATE TABLE [dbo].[Asterisks]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Code] VARCHAR(50) NOT NULL, 
-    [CreationDate] DATETIME NOT NULL, 
-    [PlaceId] INT NOT NULL 
-)
+﻿CREATE TABLE [dbo].[Asterisks] (
+    [Id]           INT          NOT NULL,
+    [Code]         VARCHAR (50) NOT NULL,
+    [CreationDate] DATETIME     NOT NULL,
+    [PlaceId]      INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Asterisks_Places] FOREIGN KEY ([PlaceId]) REFERENCES [dbo].[Places] ([Id])
+);
+
+
--- GimmeVibe.Database/dbo/Tables/Vibes.sql
+++ GimmeVibe.Database/dbo/Tables/Vibes.sql
@@ -1,11 +1,17 @@
 ﻿CREATE TABLE [dbo].[Vibes] (
-    [Id] BIGINT         IDENTITY (1, 1) NOT NULL,
-    [DeviceId]       INT         NOT NULL,
-    [EmotionId]       INT NOT NULL,
-    [PlaceId]      INT NOT NULL,
-    [AsteriskId]          INT  NULL,
-    [Comment] NCHAR(10) NULL, 
-    [CreationDate]  DATETIME  NOT NULL
-    CONSTRAINT [PK_Voting] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]           BIGINT     IDENTITY (1, 1) NOT NULL,
+    [DeviceId]     INT        NOT NULL,
+    [EmotionId]    INT        NOT NULL,
+    [PlaceId]      INT        NOT NULL,
+    [AsteriskId]   INT        NULL,
+    [Comment]      NCHAR (10) NULL,
+    [CreationDate] DATETIME   NOT NULL,
+    CONSTRAINT [PK_Voting] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Vibes_Asterisks] FOREIGN KEY ([AsteriskId]) REFERENCES [dbo].[Asterisks] ([Id]),
+    CONSTRAINT [FK_Vibes_Devices] FOREIGN KEY ([DeviceId]) REFERENCES [dbo].[Devices] ([Id]),
+    CONSTRAINT [FK_Vibes_Emotions] FOREIGN KEY ([EmotionId]) REFERENCES [dbo].[Emotions] ([Id]),
+    CONSTRAINT [FK_Vibes_Places] FOREIGN KEY ([PlaceId]) REFERENCES [dbo].[Places] ([Id])
 );
+
+
 
